f1dce5b9a345ee3049d727eab2025ee3200d7401
Pankaj Pandey <pankaj.pandey@isteams-Mac-mini-49.local>
2018-10-06 10:36:38 +0530

    commited new files

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  config: {
-    SOCKET_URL: "http://localhost:3000",
-    COMMON_URL: "https://futucare.com/api/",
-    BACKEND_URL: "http://52.224.110.147:8081",
-    API_URL: "https://futucare.com/api/api/"
-  }
-};
